af8b9cf8d71400837dc36c56a609de870b5a8abd
Fdvky1 <fdvkey@gmail.com>
2025-12-05 17:25:09 +0700

    feat: add R2 custom domain to Next.js image remote patterns
    - Allow Next.js Image component to load images from R2 custom domain
    - Add remote pattern for r2-simplepos.fdsrvr.my.id

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // https://r2-simplepos.fdsrvr.my.id/menu/a4c46414-2b62-46c5-a878-ff4701d87522.jpg
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'r2-simplepos.fdsrvr.my.id',
+      port: '',
+      pathname: '/**',
+    }]
+  }
 };
 
 export default nextConfig;
